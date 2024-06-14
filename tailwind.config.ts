f16c813ac33593519aba5d029bca75a0bfc4ad2e
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-06-14 10:21:50 -0300

    remove useless code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        notionBlack: "#191919",
+        notionWhite: "#ffffff",
+      },
+      fontFamily: {
+        sans: ["sans-serif"],
+        mono: ["IBM Plex Mono", "monospace"],
       },
     },
   },

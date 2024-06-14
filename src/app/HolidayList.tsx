85f5d0172b60dd9c38193cdf71540d8b107b2ecb
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-06-14 14:37:37 -0300

    finish?

--- src/app/HolidayList.tsx
+++ src/app/HolidayList.tsx
@@ -23,19 +23,19 @@ export function HolidayList() {
 
   function getMonthName(monthIndex: number) {
     const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro"
+    ]
 
     return months[monthIndex];
   }
@@ -52,36 +52,37 @@ export function HolidayList() {
   }
 
   return (
-    <div className="h-screen rounded-lg bg-white">
-      <header className="flex items-center gap-2 bg-[#232136] text-white">
-        <ChevronLeft
-          size={50}
-          onClick={() => handleChangeMonth("prev")}
-          className="cursor-pointer"
-        />
-
-        <span
-          className="font-bold text-3xl"
-          onClick={() => setSelectedMonth(new Date().getMonth())}
-        >
-          {getMonthName(selectedMonth)}
-        </span>
-
-        <ChevronRight
-          size={50}
-          onClick={() => handleChangeMonth("next")}
-          className="cursor-pointer"
-        />
-      </header>
-      <main>
-        <ul>
-          {filterHolidaysByMonth(data, selectedMonth).map((holiday: DataType, index: number) => (
-            <li key={index}>
-              <h1>{holiday.title} - {format(new Date(holiday.date), 'dd/MM')}</h1>
-            </li>
-          ))}
-        </ul>
-      </main>
+    <div className="p-4 h-full">
+      <div className="rounded-lg h-full max-h-52 bg-white max-w-80 flex flex-col items-center border-4 border-[#c4a7e7]">
+        <header className="flex items-center justify-center gap-2 bg-[#232136] text-white w-full">
+          <ChevronLeft
+            size={50}
+            onClick={() => handleChangeMonth("prev")}
+            className="cursor-pointer"
+          />
+          <span
+            className="font-bold text-3xl text-center w-full select-none cursor-pointer"
+            onClick={() => setSelectedMonth(new Date().getMonth())}
+          >
+            {getMonthName(selectedMonth)}
+          </span>
+          <ChevronRight
+            size={50}
+            onClick={() => handleChangeMonth("next")}
+            className="cursor-pointer"
+          />
+        </header>
+        <main className="w-full p-2">
+          <ul>
+            {filterHolidaysByMonth(data, selectedMonth).map((holiday: DataType, index: number) => (
+              <li className="flex items-center gap-2 ml-1" key={index}>
+                <div className="bg-black w-3 rounded-full h-1"></div>
+                <h1>{holiday.title} - {format(new Date(holiday.date), 'dd/MM')}</h1>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
     </div>
   )
 }
